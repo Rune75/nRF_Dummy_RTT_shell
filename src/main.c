f4722cb686d945e7330f86431e548d20f70fe276
Rune Bekkevold <rune.bekkevold@gmail.com>
2023-11-13 01:29:38 +0100

    added command modem star stop

--- src/main.c
+++ src/main.c
@@ -1,9 +1,8 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/shell/shell.h>
-//#include <zephyr/zephyr.h>
+#include <zephyr/zephyr.h>
 #include <stdlib.h>
- 
 //#include <shell/shell.h>
 
 #define LENGTH 6
@@ -11,7 +10,8 @@
 char  myval[LENGTH];
 char  psk[LENGTH];
 char  bt_token[LENGTH];
- 
+
+
 static int myval_set(const struct shell *shell, size_t argc, char *argv[])
 {
 
@@ -27,10 +27,23 @@ static int myval_set(const struct shell *shell, size_t argc, char *argv[])
  
 static int myval_get(const struct shell *shell, size_t argc, char *argv[])
 {
-    shell_fprintf(shell, SHELL_NORMAL, "%s\n", myval);
+    shell_fprintf(shell, SHELL_NORMAL, "%s ok: %s\n", argv[1], myval);
     return 0;
 }
 
+static int modem_start(const struct shell *shell, size_t argc, char *argv[])
+{
+    k_msleep(2000);
+    shell_fprintf(shell, SHELL_NORMAL, "start ok\n");
+    return 0;
+}
+
+static int modem_stop(const struct shell *shell, size_t argc, char *argv[])
+{
+    k_msleep(1000);
+    shell_fprintf(shell, SHELL_NORMAL, "stop ok\n");
+    return 0;
+}
 
 void main(void)
 {
@@ -44,6 +57,14 @@ void main(void)
             "Commands:\n"
             "\t$get <var>",
             myval_get, 2, 0),
+        SHELL_CMD_ARG(start, NULL,
+            "Commands:\n"
+            "\t$get <var>",
+            modem_start, 1, 0),
+        SHELL_CMD_ARG(stop, NULL,
+            "Commands:\n"
+            "\t$get <var>",
+            modem_stop, 1, 0),
         SHELL_SUBCMD_SET_END
         );
 
